b5bc11f8a5f2b3f3b61915597d10fb2c9b14a568
tatydfernandes <tawanefqueiroz@gmail.com>
2023-08-21 21:57:39 -0300

    Aula 02

---
+++ RedNative/IntroducaoAoJS/Aula02.js/Aula02.js
@@ -0,0 +1,15 @@
+const obj ={
+    nome: "Tawane",
+    signo: "Leão",
+    idade: 23
+}
+//Formato JSON - JavaScript Notion Object
+console.log(obj.nome, obj.signo, obj.idade)
+console.log(obj)
+console.log(typeof obj)
+
+
+let pessoa = ["Tawane", 23, "Dançarina"]
+console.log(pessoa[0], pessoa[2])
+console.log(pessoa)
+console.log(typeof pessoa)
