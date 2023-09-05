77ee9dab222ee73a5dc1acb25b22c6a5a5c2c0a6
tatydfernandes <tawanefqueiroz@gmail.com>
2023-09-04 22:17:51 -0300

    Aula de criação do projeto

---
+++ RedNative/Aula-01/meu-primeiro-app/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bom dia furacão da cpi</Text>
+      <Text>Bom dia vice miss bumbum</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
